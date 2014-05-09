6aa30c90f680ea01bd5cc5991a8296306782b847
alumne <alumne@pc-alumne>
2014-05-09 16:07:38 +0200

    prova de connexio

--- classes/connexio.php
+++ classes/connexio.php
@@ -1,7 +1,7 @@
 <?php
 class connexio extends mysqli {
 	public function __construct() {
-		parent::__construct("localhost","evai","q1w2e3r4","evai");
+		parent::__construct("mysql.hostinger.es","u881681525_cla","claclaca1","u881681525_cla");
 
 		if (mysqli_connect_error()) {
 			die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
---
+++ prova1.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'classes/connexio.php';
+
+$db = new connexio();
