fd7696993acda7e79af7d03b64a7aab23ca0a1ce
laia <laiamb@me.com>
2014-05-15 15:54:56 +0200

    Ultima actualitzacio de joc.php

--- joc.php
+++ joc.php
@@ -1,25 +1,110 @@
 <?php
-	require_once 'classes/connexio';
-	require_once 'classes/usuari.php';
-	require_once 'classes/pregunta.php';
-	require_once 'classes/partida.php'; 
-?>
+session_start();
+require_once 'classes/partida.php';
+require_once 'classes/pregunta.php';
 
+$partida = new partida();
+$nivell = 4;
+//echo $nivell;
+$pregunta = $partida->novaPregunta($nivell);
+//var_dump($pregunta);
+$respostes = $partida->getRespostes($pregunta[0]);
+
+//var_dump($respostes);
+/*if (!isset($_SESSION['nivell'])){
+	$_SESSION['nivell'] = 1;
+}*/
+
+?>
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="all" />
-<script src="js/jquery-2.1.0.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
-<scritp src="javascript/game.js"></scritp>
-<title>Claclaca</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="all" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="js/jquery-2.1.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="javascript/game.js"></script>
+
+	<title>Claclaca</title>
+	<script type="text/javascript">
+		person = { name: 'Joe Blow Job' };
+		function myfunction() {
+   		document.write(person.name);
+		}		
+	</script>
+	<script>
+	$(document).ready(function(){
+		var nivell = <?=$nivell?>;
+		if(nivell == 1){
+			$("#nivell1").click(function(){
+				obrirFinestra();				
+			});
+			$("#nivell1").hover(function()
+	                {
+	                    $("#nivell1").css("cursor","pointer");
+	                 	$("#nivell1").attr('src', 'imatges/1roig.png');
+	                }, function()
+	                {
+	                    $("#nivell1").css("cursor","default");
+	                  $("#nivell1").attr('src', 'imatges/1.png');
+	          });
+		}
+		else if(nivell == 2){
+			$("#nivell2").click(function(){
+				obrirFinestra();				
+			});
+			$("#nivell2").hover(function()
+	                {
+	                    $("#nivell2").css("cursor","pointer");
+	                 	$("#nivell2").attr('src', 'imatges/2roig.png');
+	                }, function()
+	                {
+	                    $("#nivell2").css("cursor","default");
+	                 	$("#nivell2").attr('src', 'imatges/2.png');
+	          });
+		}
+		else if(nivell == 3){
+			$("#nivell3").click(function(){
+				obrirFinestra();				
+			});
+			$("#nivell3").hover(function()
+	                {
+	                    $("#nivell3").css("cursor","pointer");
+	                  	$("#nivell3").attr('src', 'imatges/3roig.png');
+	                }, function()
+	                {
+	                    $("#nivell3").css("cursor","default");
+	                 	$("#nivell3").attr('src', 'imatges/3.png');
+	          });
+		}
+		else if(nivell == 4){
+			$("#nivell4").click(function(){
+				obrirFinestra();			
+			});
+			$("#nivell4").hover(function()
+	                {
+	                    $("#nivell4").css("cursor","pointer");
+	                    $("#nivell4").attr('src', 'imatges/4roig.png');
+	                }, function()
+	                {
+	                    $("#nivell4").css("cursor","default");
+	                 	$("#nivell4").attr('src', 'imatges/4.png');
+	          });
+		}
+		/*if(nivell > 1){
+			$("#nivell1").attr('src', 'imatges/1verd.png');
+		}*/
+		for(var i = 1; i < nivell; i++){
+			$("#nivell"+i).attr('src', 'imatges/'+i+'verd.png');
+		}
+	});
+	</script>
 </head>
+
 
 <body>
 <header>
@@ -31,7 +116,7 @@
            <div class="span12">
                <div class="span6">
                    	<div class="span4 offset2" style="margin-top:15px;">
-                   	 <h2><?php echo $_SESSION['nom'];?></h2>
+ 
                		</div>
                </div>
            </div>
@@ -42,26 +127,76 @@
 </div>
 </header>
 
-<div class="container">
+ <div class="container">
   <div class="row">
     <div class="span12">
       <div class="hero-unit center">
-          <h1>Claclaca<small><font face="Tahoma" color="red">?¿?</font></small></h1>
+          <h1><center><img src="imatges/claclaca.png" alt="Claclaca"></center></h1>
         </div>
         <br />
-      <div class="thumbnail">
-        	<canvas id="cuadro1" width="200" height="100">
-
-				Canvas not supported by your browser.
-			</canvas></p>
-
-			<script type="application/javascript" src="game.js"></script>
+        
+        <div class="btn-group btn-group-justified">
+  			<div class="btn-group">
+				<img id="nivell1" alt="1" src="imatges/1.png"> <!-- onClick="obrirFinestra()" -->
+					<div id="finestra">
+ 						<div style="font-weight: bold; text-align: left; color: #FFFFFF; padding: 5px; background-color:#006394">
+ 							<?php 
+ 							if(isset($_SESSION['ultim_resultat'])){
+ 								echo $_SESSION['ultim_resultat'];
+ 							}
+ 							else{
+ 								echo "Bienvenidos";
+ 							}
+ 							if(isset($_SESSION['puntuacio'])){
+ 								echo "<span>Puntuació: ".$_SESSION['puntuacio']."</span><br>";
+ 							}
+ 							if(isset($_SESSION['ratxa_correctes'])){
+ 								echo "<span>Ratxa d'acerts: ".$_SESSION['ratxa_correctes']."</span><br>";
+ 							}
+ 							if(isset($_SESSION['ratxa_fallades'])){
+ 								echo "<span>Ratxa d'errors: ".$_SESSION['ratxa_fallades']."</span><br>";
+ 							}
+ 							?>
+ 						</div>
+ 							<p style="padding: 5px; text-align: justify; line-height:normal">
+ 								<form action="comprovar_resposta.php" method="post">
+									<p><b><?=utf8_encode($pregunta[1])?></b></p>
+									<input type="hidden" name="nivell" value="<?=$nivell?>" />
+									<input type="hidden" name="correcta" value="<?=$respostes[0][0]?>" />
+									<?php 
+										shuffle($respostes);
+										for ($i = 0; $i < 4; $i++){
+											echo "<input type='radio' name='resposta' value='".$respostes[$i][0]."' /><label>".utf8_encode($respostes[$i][1])."</label><br>";
+										}
+									?>  
+									<input type="submit" value="Confirmar" />
+								</form>
+ 							</p>
+  						<div style="padding: 10px; background-color: #F0F0F0; text-align: center; margin-top: 44px;">
+  							<input id="btnAceptar" onclick="tancarFinestra();" name="btnAceptar" size="20" type="button" value="Aceptar" />
+ 						</div>
+					</div>     
+  			</div>
+  			<div class="btn-group">
+    			<img id="nivell2" alt="2" src="imatges/2.png">
+  			</div>
+  			<div class="btn-group">
+    			<img id="nivell3" alt="3" src="imatges/3.png">
+  			</div>
+  			<div class="btn-group">
+    			<img id="nivell4" alt="4" src="imatges/4.png">
+  			</div>
+		</div>
+		
+		<!--<script>!window.jQuery && document.write(unescape('%3Cscript src="jquery-1.7.1.min.js"%3E%3C/script%3E'))</script>
+		<script type="text/javascript" src="javascript/game.js"></script>-->
         	
       </div>
     </div>
   </div>
 </div>
 
+		
 <div id="footer">
     <div class="container">
         <div class="row">
@@ -96,6 +231,12 @@
         </div>
     </div>
 </div>
-
+<script type="text/javascript">
+var popup = <?=$_SESSION['seguir_jugant']?>;
+if(popup == 1){
+	myfunction();
+	obrirFinestra();
+}
+</script>
 </body>
 </html>
